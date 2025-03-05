c96bc0e732369f66401f2015ec677f6e9884c8ce
faisalmedsnexus <faisal.munir@medsnexus.com>
2025-03-05 13:03:32 +0500

    add menue for small screen

--- src/app/public/TopMenue.js
+++ src/app/public/TopMenue.js
@@ -1,30 +1,47 @@
-import React from 'react'
+'use client'
+import React, { useState } from "react";
 import Link from "next/link";
-
+import { Menu, X } from "lucide-react"; 
 export const TopMenue = ({ setActiveSection }) => {
-    return (
-        <div>
-            <div className='flex flex-col md:flex-row gap-5 md:gap-[50px] bg-black justify-center items-center'>
-                <Link href="/public/welcome" className="inline-block">
-                    <button className="text-[26px] font-normal text-white cursor-pointer">
-                        Free Simulator
-                    </button>
-                </Link>
-                <button className="text-[26px] font-normal text-white cursor-pointer">
-                    Access Simulator
-                </button>
-                <Link href="/public/payment" className="inline-block">
-                    <button  onClick={() => setActiveSection("products")} className="text-[26px] font-normal text-white cursor-pointer">
-                        Products
-                    </button>
-                </Link>
-                <button className="text-[26px] font-normal text-white cursor-pointer">
-                    About Us
-                </button>
-                <button className="text-[26px] font-normal text-white cursor-pointer">
-                    contact Us
-                </button>
-            </div>
-        </div>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false); 
+  return (
+    <div className="">
+      
+      <div className="md:hidden flex justify-between items-center p-4">
+        <button onClick={() => setIsOpen(!isOpen)} className="">
+          {isOpen ? <X size={30} /> : <Menu size={30} />}
+        </button>
+      </div>
+
+     
+      <div
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } md:flex flex-col md:flex-row gap-5 md:gap-[50px] justify-center items-center md:items-center bg-black`}
+      >
+        <Link href="/public/welcome" className="inline-block">
+          <button className="text-[26px] font-normal text-white cursor-pointer">
+            Free Simulator
+          </button>
+        </Link>
+        <button className="text-[26px] font-normal text-white cursor-pointer">
+          Access Simulator
+        </button>
+        <Link href="/public/payment" className="inline-block">
+          <button
+            onClick={() => setActiveSection("products")}
+            className="text-[26px] font-normal text-white cursor-pointer"
+          >
+            Products
+          </button>
+        </Link>
+        <button className="text-[26px] font-normal text-white cursor-pointer">
+          About Us
+        </button>
+        <button className="text-[26px] font-normal text-white cursor-pointer">
+          Contact Us
+        </button>
+      </div>
+    </div>
+  );
+};
