12b6dae2fe74d7a1e7728fe32efd0360874d37dd
faisalmedsnexus <faisal.munir@medsnexus.com>
2025-03-11 12:24:42 +0500

    font-family issue fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,22 +24,7 @@ export default function RootLayout({ children }) {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
-          rel="stylesheet"
-        />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100..900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&family=Quicksand:wght@300;400;700&family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
         />
       </head>
